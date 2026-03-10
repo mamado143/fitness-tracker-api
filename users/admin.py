13fba3984909f885a4eea7611c98035b2db9934e
Mohamed Dahir Mohamoud <midodahir123@gmail.com>
2026-03-10 20:55:23 +0300

    feat: setup custom user model, migrations, and JWT authentication

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'is_staff', 'is_active', 'date_joined')
+    search_fields = ('email',)
+    ordering = ('-date_joined',)
